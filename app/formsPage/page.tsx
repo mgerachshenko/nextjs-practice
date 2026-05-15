7206bf63c0cd67201bd75bde2ae076e5ed0ddec4
rrc_mgerachshenko <mgerachshenko@academic.rrc.ca>
2026-05-15 09:47:40 -0500

    changed wording from forms to surveys, deleted loading tsx file

--- app/formsPage/page.tsx
+++ app/formsPage/page.tsx
@@ -7,8 +7,8 @@ export default async function FormsPage(){
     return ( 
     <main>
         <MainHeader/>
-        <p>Here's all your forms!</p>
-        <Suspense fallback={<p>Loading forms...</p>}>
+        <p>Here's all your surveys!</p>
+        <Suspense fallback={<p>Loading surveys...</p>}>
                 <FormsList />
             </Suspense>
     </main>
